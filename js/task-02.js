1ff236a17ae6d712f80bf52ec31f637df49d0556
AleksandrNesterenko <alex1989nesterenko@gmail.com>
2021-09-09 16:58:08 +0300

    append_pravki

--- js/task-02.js
+++ js/task-02.js
@@ -8,9 +8,12 @@ const ingredients = [
   ];
   
   const getVariablesContainer = document.querySelector('#ingredients');
+  const ingredientsNewArray = [];
 
   ingredients.forEach(function(ingredient){
     const createIngredientVariables = document.createElement('li');
     createIngredientVariables.textContent = ingredient;
-    getVariablesContainer.append(createIngredientVariables);
+    ingredientsNewArray.push(createIngredientVariables)
   });
+  
+  getVariablesContainer.append(...ingredientNewArray);
